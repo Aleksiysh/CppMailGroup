d0db2b55cc2c09829520d5aea1dac15b7c3af8b3
Aleksey <aleks-sh@mail.ru>
2019-07-01 04:33:51 +0300

    Одномерные массивы

--- workWithMemory/function.cpp
+++ workWithMemory/function.cpp
@@ -7,3 +7,25 @@ int strcmp(const char * str1, const char * str2)
 	}
 	return *str1 - *str2;
 }
+
+int findmax(int * arr, int count)
+{
+	int idx = 0;
+	for (int i = 0; i < count; ++i) {
+		if (arr[i] > idx) idx = i;
+	}
+	return idx;
+}
+
+int getmax2(const int * arr, int count)
+{
+	int prev_max = arr[0]; int curr_max = arr[1];
+	if (prev_max > curr_max) { prev_max = arr[1]; curr_max = arr[0]; }
+	for (int i = 2; i < count; ++i) {
+		if (arr[i] > curr_max) {
+			prev_max = curr_max; curr_max = arr[i];
+		}
+	}
+	return prev_max;
+}
+
--- workWithMemory/function.h
+++ workWithMemory/function.h
@@ -1,4 +1,7 @@
 #pragma once
-
+/*сравнение строк*/
 int strcmp(const char *str1, const char * str2); //сравнение строк
-
+/*Индекс максимального элемента массива*/
+int findmax(int * arr, int count);
+/*второй максимальный элемента*/
+int getmax2(const int *arr, int count);
